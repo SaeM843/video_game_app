2f61a12fc50a0e559b140f3881d9b036dae50614
Alison Jeffrey <alisonnwebb@hotmail.com>
2022-08-16 18:53:24 +0900

    Made edits

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -9,15 +9,15 @@ class ApplicationPolicy
   end
 
   def index?
-    false
+    @offers = policy_scope(Offer)
   end
 
   def show?
-    false
+    true
   end
 
   def create?
-    false
+    true
   end
 
   def new?
@@ -25,7 +25,7 @@ class ApplicationPolicy
   end
 
   def update?
-    false
+    record.user == user
   end
 
   def edit?
@@ -33,7 +33,7 @@ class ApplicationPolicy
   end
 
   def destroy?
-    false
+    record.user == user
   end
 
   class Scope
