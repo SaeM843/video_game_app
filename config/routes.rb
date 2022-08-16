287956bc0c86347543d087f9bcc1c85bbaf35634
Alison Jeffrey <alisonnwebb@hotmail.com>
2022-08-16 15:53:55 +0900

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get '/index', to: 'offers#index'
+  get '/offers', to: 'offers#index'
+  get '/offers/:id', to: 'offers#show'
+  post '/bookings', to: 'bookings#create'
+  get '/bookings', to: 'bookings#index'
+
   get '/offers/new', to: 'offers#new'
   post '/offers', to: 'offers#create'
+  patch '/bookings/:id', to: 'bookings#update'
+  get '/owner/bookings', to: 'owner/bookings#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
