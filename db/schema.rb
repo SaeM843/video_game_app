2ec8a7591bee02afb54987a8c994954d5589508b
Alison Jeffrey <alisonnwebb@hotmail.com>
2022-08-16 20:15:51 +0900

    Edited but deleted changes, uncommented in application controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_16_084828) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_16_061023) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,11 +19,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_16_084828) do
     t.datetime "end_date"
     t.string "status"
     t.bigint "user_id", null: false
+    t.bigint "video_game_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "offer_id", null: false
-    t.index ["offer_id"], name: "index_bookings_on_offer_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+    t.index ["video_game_id"], name: "index_bookings_on_video_game_id"
   end
 
   create_table "offers", force: :cascade do |t|
@@ -59,8 +59,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_16_084828) do
     t.string "image_url"
   end
 
-  add_foreign_key "bookings", "offers"
   add_foreign_key "bookings", "users"
+  add_foreign_key "bookings", "video_games"
   add_foreign_key "offers", "users"
   add_foreign_key "offers", "video_games"
 end
