796d472dc1dc315059a3e1138de74cbb47e28c0a
koki0317 <80045896+koki0317@users.noreply.github.com>
2022-08-18 14:18:46 +0900

    Set the validation for booking term

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -26,7 +26,7 @@ class BookingsController < ApplicationController
     if @booking.save
       redirect_to bookings_path(@booking)
     else
-      render :new, status: :unprocessable_entity
+      render "offers/show", status: :unprocessable_entity
     end
   end
 
