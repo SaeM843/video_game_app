bf4e4fca92d004c397fe825ba423d9849bc360b8
AlexKomaha <107383667+AlexKomaha@users.noreply.github.com>
2022-08-18 17:24:56 +0900

    search

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,16 @@
 class Booking < ApplicationRecord
+
+  validates :title, presence: true
+  validates :content, presence: true
+
   belongs_to :user
   belongs_to :offer
+
+  scope :approved, -> { where(status: true) }
 
   def total_price
     (end_date - start_date).to_i * offer.price
   end
+
+
 end
