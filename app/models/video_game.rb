53a6b40eedae7d38fc598baed138377743373ca5
AlexKomaha <107383667+AlexKomaha@users.noreply.github.com>
2022-08-18 17:31:30 +0900

    search

--- app/models/video_game.rb
+++ app/models/video_game.rb
@@ -2,10 +2,5 @@ class VideoGame < ApplicationRecord
   has_many :offers
   has_many :bookings, through: :offers
   has_many :users, through: :offers
-  # include PgSearch::Model
-  # pg_search_scope :search_by_title_and_genre,
-  # against: [ :title, :genre ],
-  # using: {
-  #   tsearch: { prefix: true }
-  # }
+
 end
