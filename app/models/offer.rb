6439254a9ee7aa3a5a4a8e1b4300ae40dd074ee5
koki0317 <80045896+koki0317@users.noreply.github.com>
2022-08-18 16:07:54 +0900

    Be able to get the access to the owner/bookings page

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,6 +1,7 @@
 class Offer < ApplicationRecord
   belongs_to :user
   belongs_to :video_game
+  has_many :bookings
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
