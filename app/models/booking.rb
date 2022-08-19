b4714ab803dcebdb5b4d1c99e2462e773e897d27
Alison Jeffrey <alisonnwebb@hotmail.com>
2022-08-19 13:32:33 +0900

    Save changes - still need to edit cards

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,6 +7,6 @@ class Booking < ApplicationRecord
   scope :approved, -> { where(status: true) }
 
   def total_price
-    (end_date - start_date).to_i * offer.price
+    (end_date.day - start_date.day).to_i * offer.price
   end
 end
